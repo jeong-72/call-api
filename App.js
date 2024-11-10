15ee5956955f87b673c03264db20fefb1817e2b3
jeong-72 <jeong.lim4@studytafensw.edu.au>
2024-11-11 00:49:58 +1100

    online class practice

---
+++ App.js
@@ -0,0 +1,41 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { fetchCategories } from './utils/api';
+import { useEffect, useState } from 'react';
+
+export default function App() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(
+    () => {
+      const fetchCat = async () => {
+        try {
+          const d = await fetchCategories();
+          setCategories(d);
+        } catch (error) { }
+      };
+      fetchCat();
+    },[]);
+ 
+  
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text>Open up App.js to start working on your app!</Text>
+      {categories.map((category) => (
+        <Text key={category.id}>{category.name}</Text>
+      ))}
+
+    </View>     
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
